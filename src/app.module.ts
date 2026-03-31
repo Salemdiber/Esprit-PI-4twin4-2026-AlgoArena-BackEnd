76b37d7a651a675f88afbb0413b826dfe94baf7b
slim00077 <118676553+slim00077@users.noreply.github.com>
2026-04-01 00:14:32 +0100

    comunnity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { CommunityModule } from './community/community.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MailerModule } from '@nestjs-modules/mailer';
 
@@ -31,6 +32,7 @@ import { MailerModule } from '@nestjs-modules/mailer';
     }),
     UserModule,
     AuthModule,
+    CommunityModule,
   ],
   controllers: [AppController],
   providers: [AppService],
